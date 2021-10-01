5011d43f9fa77fa39f3edb4d614eb499cb8bb25b
Gabriel Alves <l.luis.gabriel@outlook.com>
2021-09-30 21:37:39 -0300

    feat(implem): add preference solver helper method

---
+++ src/implementation/common/PreferenceSolver.ts
@@ -0,0 +1,59 @@
+import { Alternative } from '@domain/entities/Alternative';
+import { Criterion } from '@domain/entities/Criterion';
+
+const distanceMatrix = ({
+  matrix,
+  criterionIndex,
+}: {
+  matrix: Array<Array<number>>;
+  criterionIndex: number;
+}) => {
+  return Array.from(Array(matrix.length))
+    .map(() => Array.from(Array(matrix.length)).map(() => 0))
+    .map((array, arrayIndex) =>
+      array.map(
+        (_item, itemIndex) =>
+          matrix[arrayIndex][criterionIndex] -
+          matrix[itemIndex][criterionIndex],
+      ),
+    );
+};
+
+export const preferenceSolver = ({
+  alternatives,
+  criteria,
+}: {
+  alternatives: Array<Alternative>;
+  criteria: Array<Criterion>;
+}): Array<Array<number>> => {
+  const preferenceMatrix = Array.from(Array(alternatives.length)).map(() =>
+    Array.from(Array(alternatives.length)).map(() => 0),
+  );
+
+  criteria.forEach((criterion, criterionIndex) => {
+    const distanceArray = distanceMatrix({
+      matrix: alternatives.map((alternative) => alternative.evaluations),
+      criterionIndex,
+    }).map((array, arrayIndex) =>
+      array.map((item, itemIndex) =>
+        arrayIndex !== itemIndex
+          ? criterion.preferenceFunction(
+              item *
+                ({ maximize: 1, minimize: -1 } as Record<string, number>)[
+                  criterion.objective
+                ],
+            )
+          : 0,
+      ),
+    );
+
+    preferenceMatrix.forEach((array, arrayIndex) => {
+      array.forEach((_item, index) => {
+        preferenceMatrix[arrayIndex][index] +=
+          criterion.weight * distanceArray[arrayIndex][index];
+      });
+    });
+  });
+
+  return preferenceMatrix;
+};
